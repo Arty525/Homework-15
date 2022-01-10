409af1c0f4a882ddfe7b1d768219ecf0ed54a2ca
Andrey <89473462+Arty525@users.noreply.github.com>
2022-01-10 17:01:58 +0300

    Add files via upload

---
+++ ex2/ex2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	vector <int> a = { 2, 7, 11, 15 };
+	int res = 9;
+	int x = 0;
+	int y = 0;
+
+	for (int i = 0; i < a.size() - 1; ++i) {
+		for (int j = i+1; j < a.size(); ++j) {
+			if (a[i] + a[j] == res) {
+				x = i;
+				y = j;
+			}
+		}
+	}
+
+	cout << "X: " << x << ", Y: " << y << ", Summ: " << res << endl;
+
+	return 0;
+}
---
+++ ex3/ex3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector <int> a = { 0 };
+	int x = 0;
+
+	for (int i = 0; x != -2; ++i) {
+		cout << "X: ";
+		cin >> x;
+		a[i] = x;
+		if (a.size() > 1 && a[i] < a[i - 1]) {
+			for (int j = a.size() - 1; x < a[j - 1]; --j) {
+				a[j] = a[j - 1];
+				a[j - 1] = x;
+			}
+		}
+		a.resize(a.size() + 1);
+	}
+	for (int i = 0; i < a.size(); ++i) {
+		cout << a[i] << ", ";
+	}
+
+
+	return 0;
+}
