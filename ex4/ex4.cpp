74f845d89a43cc7abd7ee612d7cb531dbc3dbc66
Andrey <89473462+Arty525@users.noreply.github.com>
2022-01-14 15:55:43 +0300

    Add files via upload

--- ex4/ex4.cpp
+++ ex4/ex4.cpp
@@ -7,15 +7,17 @@ int main()
 {
 	vector <int> a = { -100,-50, -5, 1, 10, 150 };
 	int x = 0;
-	int y = a[0];
 	for (int i = 0; i < a.size(); ++i) {
 		if (a[i] > 0 && a[i] < fabs(a[x])) x = i;
-		if (fabs(a[i]) > fabs(y)) y = a[i];
 	}
-	cout << a[x]<< ", ";
-	for (int i = 1; i < 3; ++i) {
-		fabs(a[x - i]) < fabs(a[x + i]) ? cout << a[x - i] << ", " << a[x + i] << ", " : cout << a[x + i] << ", " << a[x - i] << ", ";
+	for (int i = x; i < a.size(); ++i) {
+		if (fabs(a[a.size() - i]) < fabs(a[i])) {
+			for (int j = a.size() - i; j >= 0 && fabs(a[j]) < fabs(a[i]); --j) {
+				cout << a[j] << " ";
+			}
+		}
+		cout << a[i] << " ";
 	}
-	cout << y;
+
 	return 0;
 }
