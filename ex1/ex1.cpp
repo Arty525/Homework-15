05f02f45f0c95a9ecbff0479c79cb759e63d0d67
Andrey <89473462+Arty525@users.noreply.github.com>
2022-01-17 15:41:56 +0300

    Add files via upload

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -6,24 +6,24 @@ using namespace std;
 int main() {
 
 	vector <int> a = { -2,1,-3,4,-1,2,1,-5,4 };
-	int max = 0;
-	int min = 0;
+	int start = 0;
 	int current = 0;
 	int summ = 0;
 	int x = 0;
 	int y = 0;
 
-	for (int i = 0; i < a.size()-1; ++i) {
-		if (a[i] > max) {
-			max = a[i];
-			x = i;
-			current = 0;
-		}
+	for (int i = 0; i < a.size(); ++i) {
 		current += a[i];
-		if (a[i] < min) {
-			min = a[i];
-			y = i-1;
-			summ = current - a[i];
+		if (current > summ) {
+			summ = current;
+			y = i;
+			x = start + 1;
+
+		}
+		if (i == a.size() - 1 && start < a.size()-3) {
+			++start;
+			i = start;
+			current = 0;
 		}
 	}
 
