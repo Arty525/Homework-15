970393084f2f29ec1d24e091befaaac551fdb775
Andrey <89473462+Arty525@users.noreply.github.com>
2022-01-18 09:41:40 +0300

    Add files via upload

--- ex1/ex1.cpp
+++ ex1/ex1.cpp
@@ -8,22 +8,22 @@ int main() {
 	vector <int> a = { -2,1,-3,4,-1,2,1,-5,4 };
 	int start = 0;
 	int current = 0;
-	int summ = 0;
+	int summ = a[0];
 	int x = 0;
 	int y = 0;
 
 	for (int i = 0; i < a.size(); ++i) {
 		current += a[i];
-		if (current > summ) {
+		if (current > summ && i > start) {
 			summ = current;
 			y = i;
-			x = start + 1;
+			x = start;
 
 		}
-		if (i == a.size() - 1 && start < a.size()-3) {
+		if (i == a.size() - 1 ) {
 			++start;
 			i = start;
-			current = 0;
+			current = a[start];
 		}
 	}
 
